5be8122f8621a537695fb93963dc48cbba46aef3
Vasil Bazhiv <vanyakuratov70@gmail.com>
2022-09-18 21:57:15 +0300

    new-for

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Скільки фільмів ви подивились', '');
+const numberOfFilms = prompt('Скільки фільмів ви подивились', '')
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -7,12 +7,21 @@ const personalMovieDB = {
 	genres: [],
 	private: false,
 };
-const a = prompt('Напишіть один із останіх переглянутих фільмів', ''),
-	b = prompt('Дайте свою оцінку'),
-	c = prompt('Напишіть один із останіх переглянутих фільмів', ''),
-	d = prompt('Дайте свою оцінку');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+for (let i = 1; i <= 2; i++) {
+	const a = prompt('Напишіть один із останіх переглянутих фільмів', '');
+	const b = prompt('Дайте свою оцінку')
+	if (a == '' || b == '') {
+		i = 1;
+	} else if (a.length >= 50 || b.length >= 50) {
+		i = 1
+	}
+	personalMovieDB.movies[a] = b;
+}
+if (numberOfFilms < 10) {
+	alert('Мало фильмов')
+} else if (numberOfFilms > 10 && numberOfFilms < 30) {
+	alert('Класичний зритель')
+} else {
+	alert('Киноман')
+}
